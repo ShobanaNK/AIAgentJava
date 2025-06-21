e11578929baa1c6902bbb4c9c013aa052e72936c
ShobanaNK <snk.shobana@gmail.com>
2025-06-21 13:41:36 +0530

    Initial version - setUp, successful logs, openai api configuration

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner onSuccessfulLoad() {
+		return args -> {
+			logger.debug("Demo Application successfully started.");
+		};
+	}
+
+}
